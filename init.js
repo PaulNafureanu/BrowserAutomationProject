4ca2ec8edbea3e820b5f20e5073e6ed22c012cf3
PaulNafureanu <paul.nafureanu@gmail.com>
2023-05-27 03:17:52 +0300

    Init commit

---
+++ init.js
@@ -0,0 +1,4 @@
+"use strict";
+async function init() { }
+init();
+//# sourceMappingURL=init.js.map
---
+++ selenium-aliases.d.ts
@@ -0,0 +1,7 @@
+declare module "selenium3.6" {
+  export * from "selenium3";
+}
+
+declare module "selenium4.9" {
+  export * from "selenium4";
+}
